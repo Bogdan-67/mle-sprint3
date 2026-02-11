7abd40faa90fd2ee6563ad7bbf53bd4ed270ed06
Bogdan-67 <tugay.bogdan03@mail.ru>
2026-02-11 14:03:51 +0000

    добавил сбор метрик в prometheus

--- fastapi_monitoring/main_app/main.py
+++ fastapi_monitoring/main_app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Histogram
+from prometheus_client import Counter
 import numpy as np
 
 # создание экземпляра FastAPI приложения
@@ -10,10 +12,26 @@ app = FastAPI()
 instrumentator = Instrumentator()
 instrumentator.instrument(app).expose(app)
 
+# main_app_predictions — объект метрики
+main_app_predictions = Histogram(
+    # имя метрики
+    "main_app_predictions",
+    # описание метрики
+    "Histogram of predictions",
+    # указываем корзины для гистограммы
+    buckets=(1, 2, 4, 5, 10)
+) 
+
+positive_predictions_counter = Counter("positive_predictions", "Count of positive predictions")
+
 # предсказания
 @app.get("/predict")
 def predict(x: int, y: int):
     #print(x)
     np.random.seed(int(abs(x)))
     prediction = x+y + np.random.normal(0,1)
+    main_app_predictions.observe(prediction)
+    
+    if prediction > 0:
+        positive_predictions_counter.inc()
     return {'prediction': prediction}
